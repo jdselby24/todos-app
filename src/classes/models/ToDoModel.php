9e46a5b6b7bc4d461a6a9f7e06c8296acbeb995c
jdselby24 <jdselby24@gmail.com>
2019-11-14 14:12:27 +0000

    Added addToDo function to ToDoModel

--- src/classes/models/ToDoModel.php
+++ src/classes/models/ToDoModel.php
@@ -9,9 +9,26 @@ class ToDoModel
 
     private $db;
 
+    /**
+     * Constructor
+     *
+     * @param PDO $db PDO Database object
+     */
     public function __construct(PDO $db)
     {
         $this->db = $db;
     }
 
+    /**
+     * Function for adding a todo to the DB with a name
+     *
+     * @param string $name The name of the todo
+     * @return boolean DB Success
+     */
+    public function addToDo(string $name) : bool {
+        $statement = "INSERT INTO `todos` (`name`) VALUES (:name)";
+        $query = $this->db->prepare($statement);
+        $query->bindParam(":name", $name, PDO::PARAM_STR, 512); 
+        return $query->execute();
+    }
 }
