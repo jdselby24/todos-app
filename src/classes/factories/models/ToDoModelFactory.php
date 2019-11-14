aa70b41bf94bd3c4c7f2236db84f2ef6276ac90a
jdselby24 <jdselby24@gmail.com>
2019-11-14 14:15:35 +0000

    Added ToDoModel factory doc blocs

--- src/classes/factories/models/ToDoModelFactory.php
+++ src/classes/factories/models/ToDoModelFactory.php
@@ -7,6 +7,12 @@ use Todo\models\ToDoModel;
 
 class ToDoModelFactory
 {
+    /**
+     * Creates a new ToDoModel with DB dependency
+     *
+     * @param ContainerInterface $container DIC
+     * @return ToDoModel The new ToDoMode object
+     */
     public function __invoke(ContainerInterface $container) : ToDoModel
     {
         $db = $container->get('db');
