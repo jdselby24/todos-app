a0b8859b68b93cf3a825b15c2f18ca5bcbdf157d
jdselby24 <jdselby24@gmail.com>
2019-11-14 14:58:19 +0000

    GetAllToDosController route

--- src/routes.php
+++ src/routes.php
@@ -7,14 +7,7 @@ use Slim\Http\Response;
 return function (App $app) {
     $container = $app->getContainer();
 
-    $app->get('/', function (Request $request, Response $response, array $args) use ($container) {
-        // Sample log message
-        $container->get('logger')->info("Slim-Skeleton '/' route");
-
-        // Render index view
-        return $container->get('renderer')->render($response, 'index.phtml', $args);
-    });
-
+    $app->get('/', 'GetAllToDosController');
     $app->post('/add', 'AddToDoController');
 
 };
