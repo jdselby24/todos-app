353a6ef08c94e1c2299dbb742ef7941559b08150
jdselby24 <jdselby24@gmail.com>
2019-11-14 14:42:13 +0000

    Added getAllToDos method to ToDoModel

--- src/classes/models/ToDoModel.php
+++ src/classes/models/ToDoModel.php
@@ -26,9 +26,22 @@ class ToDoModel
      * @return boolean DB Success
      */
     public function addToDo(string $name) : bool {
-        $statement = "INSERT INTO `todos` (`name`) VALUES (:name)";
+        $statement = "INSERT INTO `todos` (`name`) VALUES (:name);";
         $query = $this->db->prepare($statement);
         $query->bindParam(":name", $name, PDO::PARAM_STR, 512); 
         return $query->execute();
+    }
+
+    /**
+     * Fetch all todos from the DB
+     *
+     * @return array Array containing the result of the DB query
+     */
+    public function getAllToDos() : array {
+        $statement = "SELECT `id`,`name`,`completed` FROM `todos`;";
+        $query = $this->db->prepare($statement);
+        $success = $query->execute();
+        $data = $query->fetchAll();
+        return ["success" => $success, "data_todos" => $data];
     }
 }
--- src/dependencies.php
+++ src/dependencies.php
@@ -27,6 +27,7 @@ return function (App $app) {
      */
     $container['db'] = function($c) : PDO {
         $db = new PDO('mysql:host=127.0.0.1;dbname=todo', 'root', 'password');
+        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         return $db;
     };
 
