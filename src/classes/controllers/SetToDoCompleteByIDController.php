f273b6493bed37fda6e43ba30b346d3dfca69bb3
jdselby24 <jdselby24@gmail.com>
2019-11-14 15:12:03 +0000

    Added SeToDoCompleteByIDController

---
+++ src/classes/controllers/SetToDoCompleteByIDController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Todo\controllers;
+
+use Todo\models\ToDoModel;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class SetToDoCompleteByID
+{
+    /**
+     * A ToDoModel
+     *
+     * @var ToDoModel
+     */
+    private $model;
+
+    /**
+     * Constructor
+     *
+     * @param ToDoModel $model ToDoModel to be injected
+     */
+    public function __construct(ToDoModel $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * Method for setting a todo complete by its ID
+     *
+     * @param Request $req Request object
+     * @param Response $res Response object
+     * @param array $args Arguments
+     * @return Response Response object
+     */
+    public function __invoke(Request $req, Response $res, array $args) : Response
+    {
+        $todo_id = $req->getParsedBody()['todo_id'];
+        $success = $this->model->setToDoCompleteByID($todo_id);
+
+        $response = [
+            "success" => $success,
+            "message" => "",
+            "request" => $req->getParsedBody()
+        ];
+
+        if($success === true) {
+            $response['message'] = "Successfully set Todo: $todo_id completed";
+            return $res->withJson($response, 200);
+        } else {
+            $response['message'] = "DATABASE ERROR: Could not set todo complete";
+            return $res->withJson($response, 500);
+        }
+    }
+}
